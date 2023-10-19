ac0af551faac6c1304cdf4ce6bb84845f6f23ae8
CeliaSTEM <celia@stemgranada.com>
2023-10-19 19:22:29 +0200

    Ej5 Resuelto

--- Ej5AreaTriangulo.java
+++ Ej5AreaTriangulo.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
-
-public class Plantilla{
+/*
+    Escribe una aplicación que solicite por consola dos números reales que corresponden a la base y la altura 
+    de un triángulo. Deberá mostrarse su área, comprobando que los números introducidos por el usuario no son
+    negativos, algo que no tendría sentido.
+*/
+public class Ej5AreaTriangulo{
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        int num;
+        double base;
+        double altura;
+        double area;
 
-        System.out.print("");
-        num = sc.nextInt();
+        System.out.print("Dime un número real para la base de un triángulo: ");
+        base = sc.nextDouble();
+        System.out.print("Dime un número real para la altura de un triángulo: ");
+        altura = sc.nextDouble();
+
+        if (base <= 0 || altura <= 0){
+            System.out.print("No podmeos calcular el area con esos valores...");
+        }else{
+            area = base*altura/2;
+            System.out.print("El area del triángulo es " + area + ".");
+        }
+
     }
 }
