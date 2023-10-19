4831c452ff6f548b86d6d33ec5dff46a3a25edb8
CeliaSTEM <celia@stemgranada.com>
2023-10-19 18:35:03 +0200

    Base para ejercicio5

---
+++ Ej5AreaTriangulo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Plantilla{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.print("");
+        num = sc.nextInt();
+    }
+}
