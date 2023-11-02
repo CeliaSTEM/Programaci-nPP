847f3e7fc69d3ba764a52de9f1737296a7544ec6
CeliaSTEM <145722929+CeliaSTEM@users.noreply.github.com>
2023-11-02 19:48:38 +0100

    Explicación bucles

---
+++ bucles.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class bucles{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edad = 0;
+
+        while(edad >= 0 ){
+            System.out.println("Indica la edad de la siguiente persona [-1 para salir]: ");
+            edad = sc.nextInt();
+            if (edad < 18){
+                System.out.println("Es menor de edad!");
+            }else{
+                System.out.println("Es mayor de edad!");
+            }
+        }
+
+        // ---------------------------------------------------------
+
+        int contador = 0;
+        while (contador < 100){
+            System.out.println(contador);
+            contador++;
+        }
+
+        // ---------------------------------------------------------
+
+        for (int cont = 0; cont < 100 ; cont++){
+            System.out.println(cont);
+        }
+        
+    }
+}
